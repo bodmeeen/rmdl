3c33d6a4598ad03c68d8ae43fde5ea9587716b34
bodmeeen <maks.demianp@gmail.com>
2026-07-24 14:58:29 +0300

    add the ability to download only one song, and moved the link input to a separate function

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,14 @@ use std::{io::Read, path::Path, process::Command};
 use::std::io;
 
 fn main() {
-    let dir_path = "./Music/";
-    if !Path::new(dir_path).exists() {
-        match fs::create_dir(dir_path) {
-            Ok(_) => println!("Створено папку {}", dir_path),
-            Err(e) => eprintln!("Помилка при створенні папки: {}", e)
-        }
+    let folders = ["./Music/Albums", "./Music/Singles", "./Music/Playlists"];
+
+    for folder in folders {
+        fs::create_dir_all(folder).expect(&format!("Помилка при створенні папки {}", folder));
     }
 
     println!("\nВиберіть дію:\n 1 - альбом (з обкладинкою, окрема папка, mp3, без зайвих символів)
-                \n 2 - одна пісня (те ж саме що і в альбомі, тільки без окремої папки)");
+                \n 2 - одна пісня (те ж саме що і в альбомі)");
 
     let mut input_text = String::new();
     
@@ -22,34 +20,54 @@ fn main() {
 
     match input_text.trim().chars().next() {
         Some('1') => download_album(),
-        Some('2') => todo!(),
+        Some('2') => download_single_song(),
         // Some('q') => break,
         Some(other_ch) => println!("Команди {} не існує", other_ch),
         None => println!("Натиснуто Enter на порожньому місці!")
     }
-    
 }
 
-fn download_album(){
-    println!("Вставте посилання на альбом: ");
-    let mut url: String = String::new();
-    io::stdin()
-        .read_line(&mut url)
-        .expect("Error in reading input");
-    
-    let clean_url = url.trim();
+fn download_single_song() {
+    let clean_url = input_url();
 
-    println!("\n Завантаження почато... ");
+    println!("\n Завантаження почато... "); 
+
+    // запуск основного завантаження    
+    let status = Command::new("yt-dlp")
+        .arg("-x")
+        .arg("--embed-thumbnail")
+        .arg("--audio-format").arg("mp3")
+        .arg("--embed-metadata") // теги
+        .arg("--no-playlist") // завантажити тільки цю пісню якщо посилання на альбом чи плейлист
+        .arg("-o").arg(format!("./Music/Singles/%(title)s.%(ext)s")) // папка та чиста назва
+        .arg(&clean_url)
+        .status()
+        .expect("Помилка у завантаженні");
+
+    if status.success() {
+        println!("Успішно завантажено!");
+        println!("Шлях до папки: ./Music/Singles");
+        let files = show_music_files("Singles");
+        print_files_list(&files);
+    } else {
+        println!("Виникла помилка під час завантаження");
+    }
+}
+
+fn download_album() {
+    let clean_url = input_url();
 
     // запис назви альбому який створиться
     let yt_output = Command::new("yt-dlp")
-        .arg("--print")
-        .arg("%(playlist_title)s")
-        .arg("--playlist-items").arg("1")
-        .arg(clean_url)
-        .output()
-        .expect("Не вдалося запустити yt-dlp");
+    .arg("--print")
+    .arg("%(playlist_title)s")
+    .arg("--playlist-items").arg("1")
+    .arg(&clean_url)
+    .output()
+    .expect("Не вдалося запустити yt-dlp");
 
+    println!("\n Завантаження почато... ");
+    
     // перетворення виводу в текст
     let full_output = String::from_utf8_lossy(&yt_output.stdout).trim().to_string();
 
@@ -63,10 +81,9 @@ fn download_album(){
         .arg("--audio-format").arg("mp3")
         .arg("--embed-metadata") // теги
         .arg("-o").arg(format!("./Music/%(playlist_title)s/%(title)s.%(ext)s")) // папка та чиста назва
-        .arg(clean_url)
+        .arg(&clean_url)
         .status()
         .expect("Помилка у завантаженні");
-
 
     if status.success() {
         println!("Успішно завантажено!");
@@ -102,4 +119,13 @@ fn print_files_list(files: &[String]) {// отримання зрізу (slice) 
     for (index, name) in files.iter().enumerate() {
         println!("{} -> {}", index + 1, name);
     }
+}
+
+fn input_url() -> String{
+    println!("Вставте посилання: ");
+        let mut url: String = String::new();
+        io::stdin()
+            .read_line(&mut url)
+            .expect("Error in reading input");
+        url.trim().to_string()
 }
